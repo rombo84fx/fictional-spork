57fed0cbd532214717ac015a5fd4c63aa1cf5c4b
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2016-05-26 10:49:24 -0500

    A Comparison of Microsoft Web Technologies Projects

---
+++ DemoMVC/DemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoMVC.Models;
+
+namespace DemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult About(string userData)
+        {
+            ViewBag.Message = userData;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View(TechItem.GetItems());
+        }
+    }
+}
